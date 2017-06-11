a08d5e5325d813deda5a95f0166f5e4016253fc0
Sri Venkatesh <lps.162@gmail.com>
2017-06-11 20:26:11 +0530

    removed selector for routed components

--- limetools-router/src/app/about/about.component.ts
+++ limetools-router/src/app/about/about.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'about-page',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-  
+
   constructor() { }
 
   ngOnInit() {
--- limetools-router/src/app/contact/contact.component.ts
+++ limetools-router/src/app/contact/contact.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'contact-page',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-  title: string = "This is the contact page";
-  
+  title = 'This is the contact page';
+
   constructor() { }
 
   ngOnInit() {
